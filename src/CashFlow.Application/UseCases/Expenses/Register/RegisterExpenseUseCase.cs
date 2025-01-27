a449690c03b87caa52ce4276fc3ae7e59f686bfc
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-01-27 20:31:41 -0300

    feat: valid request in RegisterExpenseUseCase

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,9 +7,37 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        // TO DO VALIDATIONS
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
-        
+
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title field is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greather than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment type is not valid");
+
+        }
     }
 }
