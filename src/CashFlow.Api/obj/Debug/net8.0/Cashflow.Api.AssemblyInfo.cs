b638f45ac71492db1059f84931d7ee74bb3697a5
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2025-01-28 08:35:50 -0300

    feat: FluentValidation

--- src/CashFlow.Api/obj/Debug/net8.0/Cashflow.Api.AssemblyInfo.cs
+++ src/CashFlow.Api/obj/Debug/net8.0/Cashflow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a449690c03b87caa52ce4276fc3ae7e59f686bfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93851ec4e93e90010d8ac86c73aa7686f09b4a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
